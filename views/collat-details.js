aad5845bcfa404887986d2cfc122fd7573270b56
jimmiewiththatj <jim.wenjin.luo@gmail.com>
2022-07-24 00:51:33 -0400

    jhg

--- views/collat-details.js
+++ views/collat-details.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-
-    $("#nft-sale").click(function(){
-        $("body").load("/collat-details.html");
+    $("#finalize").click(function(){
+        $("body").load("/collat-confirm.html");
     });
-  
 
+    /*
     var slider = document.getElementById("myRange");
     var output = document.getElementById("demo");
     output.innerHTML = slider.value;
+    
 
     slider.oninput = function() {
-        output.innerHTML = this.value;
+    output.innerHTML = this.value;
     }
+    */
 
+    
 });
